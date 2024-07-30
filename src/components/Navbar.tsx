5d33632748a2383687c8a67304326b34c576af96
MohammadAmmar11 <ammarqasimpakistan123@gmail.com>
2024-07-30 06:46:36 +0500

    edited

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,8 +8,8 @@ export default function Navbar() {
         <div className="flex w-fit gap-10 mx-auto font-medium py-4 text-blackish">
           <Link className="navbar__link relative" href="#">HOME</Link>
           <Link className="navbar__link relative" href="#">CATEGORIES</Link>
-          <Link className="navbar__link relative" href="#">MEN'S</Link>
-          <Link className="navbar__link relative" href="#">WOMEN'S</Link>
+          <Link className="navbar__link relative" href="#">MEN&apos;S</Link>
+          <Link className="navbar__link relative" href="#">WOMEN&apos;S</Link>
           <Link className="navbar__link relative" href="#">JEWELRY</Link>
           <Link className="navbar__link relative" href="#">PERFUME</Link>
           <Link className="navbar__link relative" href="#">BLOGS</Link>
